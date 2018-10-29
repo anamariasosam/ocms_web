e31dbb04c735f1e733ae9e9586b75a25e82ac760
Ana María <anamariasosam@gmail.com>
2018-10-29 13:06:55 -0500

    add login style

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 const LoginHeader = () => (
-	<div>
-		<span>Bienvenido</span>
-		{ ' | ' }
-		<a href="/inicio">Iniciar Sesión</a>
+	<div className="login--container">
+		<span className="login--welcome">Bienvenido</span>
+		<span className="login--pipe">|</span>
+		<a href="/inicio" className="login--link">Iniciar Sesión</a>
 	</div>
 )
 
