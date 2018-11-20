1e18269e4885d8e68811ab00a29687325087122e
Ana María <anamariasosam@gmail.com>
2018-11-19 21:52:59 -0500

    add create form^

--- src/views/modules/AcademicSchedule/AgendaCreateForm.js
+++ src/views/modules/AcademicSchedule/AgendaCreateForm.js
@@ -1,6 +1,11 @@
 import React, { Component, Fragment } from 'react'
+import axios from 'axios'
+import { Redirect } from 'react-router-dom'
 import Success from '../../../components/Success'
 import Error from '../../../components/Error'
+import PACKAGE from '../../../../package.json'
+
+const API_URL = PACKAGE.config.api[process.env.NODE_ENV]
 
 class AgendaCreateForm extends Component {
   constructor(props) {
@@ -10,35 +15,64 @@ class AgendaCreateForm extends Component {
       error: false,
       success: false,
       message: 'Creado con éxito',
+      saved: false,
+      tipos: ['Parcial', 'Final', 'Foro'],
     }
 
-    this.name = React.createRef()
+    this.nombre = React.createRef()
     this.fechaInicio = React.createRef()
     this.fechaFin = React.createRef()
+    this.tipo = React.createRef()
 
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleSubmit(e) {
     e.preventDefault()
-    console.log(this.name.current.value)
-    console.log(this.fechaInicio.current.value)
-    console.log(this.fechaFin.current.value)
+    const nombre = this.nombre.current.value
+    const fechaInicio = this.fechaInicio.current.value
+    const fechaFin = this.fechaFin.current.value
+    const tipo = this.tipo.current.value
+    const calendarioId = '5be9a32d93638328b927d67a'
 
-    this.name.current.value = ''
-    this.fechaInicio.current.value = ''
-    this.fechaFin.current.value = ''
-
-    this.showSuccessMessage()
+    axios
+      .post(`${API_URL}/programaciones`, {
+        data: {
+          nombre,
+          fechaInicio,
+          fechaFin,
+          tipo,
+          calendarioId,
+        },
+      })
+      .then(res => {
+        if (res.status === 200) {
+          this.nombre.current.value = ''
+          this.fechaInicio.current.value = ''
+          this.fechaFin.current.value = ''
+          this.toggleAlert()
+        }
+      })
   }
 
-  showSuccessMessage() {
-    this.setState({
-      success: true,
-    })
+  toggleAlert() {
+    this.setState(
+      {
+        success: true,
+      },
+      () => {
+        setTimeout(() => {
+          this.setState({
+            success: false,
+            saved: true,
+          })
+        }, 1000)
+      },
+    )
   }
 
   render() {
+    const { error, success, message, saved } = this.state
     return (
       <Fragment>
         <h2>Gestionar Programación</h2>
@@ -46,10 +80,19 @@ class AgendaCreateForm extends Component {
         <div className="form--container">
           <h3 className="form--title">Crear Programación</h3>
           <form onSubmit={this.handleSubmit}>
-            <label htmlFor="name" className="required label">
+            <label htmlFor="nombre" className="required label">
               Nombre:
             </label>
-            <input type="text" id="name" className="input" ref={this.name} required />
+            <input type="text" id="nombre" className="input" ref={this.nombre} required />
+
+            <label htmlFor="tipo" className="required label">
+              Tipo de Evento:
+            </label>
+            <select id="tipo" ref={this.tipo} className="input select--input">
+              {this.state.tipos.map(tipo => (
+                <option key={tipo}>{tipo}</option>
+              ))}
+            </select>
 
             <label htmlFor="fechaInicio" className="required label">
               Fecha Inicio:
@@ -66,8 +109,9 @@ class AgendaCreateForm extends Component {
             </div>
           </form>
 
-          {this.state.error && <Error description={this.state.message} />}
-          {this.state.success && <Success description={this.state.message} />}
+          {error && <Error description={message} />}
+          {success && <Success description={message} />}
+          {saved && <Redirect to="/calendarioAcademico/realizarProgramacion/show/2017-1" />}
         </div>
       </Fragment>
     )
