eccec1f4319643ee82198cab474a1ed876e1e401
Ana Sosa <anamariasosam@gmail.com>
2018-12-03 16:32:20 -0500

    rename index to auth

--- src/components/auth/LoginForm.js
+++ src/components/auth/LoginForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Field, reduxForm } from 'redux-form'
 import { Link } from 'react-router-dom'
-import { loginUser } from '../../actions'
+import { loginUser } from '../../actions/auth'
 import Error from '../Error'
 
 const form = reduxForm({
