65fbb2ae6c5f7753aaab523124a38905dd91f0a1
Ana Sosa <anamariasosam@gmail.com>
2018-12-04 18:59:55 -0500

    add activeclass

--- src/components/SideBarElement.js
+++ src/components/SideBarElement.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const SideBarElement = ({ name, path }) => {
-  const active = path.split('/')[2] === window.location.pathname.split('/')[2] ? 'active' : ''
   return (
     <li>
-      <Link to={path} className={`reset--link ${active}`}>
+      <NavLink to={path} className="reset--link" activeClassName="active">
         {name}
-      </Link>
+      </NavLink>
     </li>
   )
 }
